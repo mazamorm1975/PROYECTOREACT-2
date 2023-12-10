6f94db505f22cca4043c0ac6dde63a6cc1d03756
mazamora1975 <mzamora114@hotmail.com>
2023-12-10 13:27:16 -0600

    Actualizacion del curso en el Modelo 40. Uso de PropTypes

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,11 +1,20 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prop-types
-export function HolaMundo({user, id}){
+export const HolaMundo =({user, id, title ='Hola mundo'})=>{
 
     return (
     <>
-    <h1>Hola Que Tal a todos</h1>
-    <div>Como estas  {user} , tu ID en fedex hasta el 2016 era {id}</div>
+    <h1>{title}</h1>
+    <div>Como estas  {user.name} {user.lastName} , tu ID en fedex hasta el 2016 era {id}</div>
     </>
   );
+}
+
+HolaMundo.propTypes = {
+  user: PropTypes.object.isRequired,
+  title: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 }
